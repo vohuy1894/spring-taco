c8a45864372700c13908bbebc507146297238cbd
Huy Vo <vohuy1894@gmail.com>
2022-07-18 16:50:03 -0500

    Init spring project

---
+++ src/main/java/com/example/demo/SpringTacoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTacoApplication.class, args);
+	}
+
+}
